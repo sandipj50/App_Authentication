35f3f40f5eb8a637d6c69921986fbeadc6ff80b2
Sandip Jha <sandipjha50@gmail.com>
2021-08-22 01:13:21 +0530

    Authentication

---
+++ passport-config.js
@@ -0,0 +1,34 @@
+
+const passport = require('passport')
+const LocalStratergy = require('passport-local')
+const bcrypt = require('bcrypt');
+function initialize(passport,getUserByEmail,getUserById){
+    const authenticateUser = async(email,password,done)=>{
+        const user = getUserByEmail(email);
+        if(user==null){
+            return done(null,false,{message:'No user wiith that email'});
+        }
+
+        try{
+            if(await bcrypt.compare(password,user.password)){
+                return done(null,user);
+            }else{
+                return done(null,false,{message:'Passeord incorrect'})
+            }
+
+        }catch(e){
+            return done(e);
+
+        }
+
+    }
+    passport.use(new LocalStratergy({usernameField:'email'},
+    authenticateUser));
+    passport.serializeUser((user,done)=> done(null,user.id));
+    passport.deserializeUser((id,done)=>{
+        return done(null,getUserById(id))
+    });
+
+}
+
+module.exports = initialize
